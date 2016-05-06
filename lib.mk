83953998479602689d0cc496b77d4bfa8b91458a
Kevin van Zonneveld <vanzonneveld@gmail.com>
2016-05-06 11:12:59 +0200

    Less verbosity

--- lib.mk
+++ lib.mk
@@ -4,7 +4,7 @@ define dm_packets
 PACKETS := $(basename $(notdir $(wildcard $(DM_PACKETS_DIR)/*.sh)))
 ALL:
 	@$(MAKE) $$(PACKETS)
-	cat $(wildcard $(DM_BUILD_DIR)/*/build.fingerprint) |openssl sha1 | cut -d ' ' -f 2 > $(DM_STACK_DIR)/build.combined.fingerprint
+	@cat $(wildcard $(DM_BUILD_DIR)/*/build.fingerprint) |openssl sha1 | cut -d ' ' -f 2 > $(DM_STACK_DIR)/build.combined.fingerprint
 
 $$(PACKETS):
 	@source $(DM_DIR)/lib.sh && dm_build $$@ $$^
