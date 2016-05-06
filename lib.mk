0a6d3fd2c0e299156a7771130f150ba3c1fd569c
Kevin van Zonneveld <vanzonneveld@gmail.com>
2016-05-06 11:05:11 +0200

    Add a global fingerprint to stack directories

--- lib.mk
+++ lib.mk
@@ -2,7 +2,10 @@ SHELL := /bin/bash
 
 define dm_packets
 PACKETS := $(basename $(notdir $(wildcard $(DM_PACKETS_DIR)/*.sh)))
-ALL: $$(PACKETS)
+ALL:
+	@$(MAKE) $$(PACKETS)
+	cat $(wildcard $(DM_BUILD_DIR)/*/build.fingerprint) |openssl sha1 | cut -d ' ' -f 2 > $(DM_STACK_DIR)/build.combined.fingerprint
+
 $$(PACKETS):
 	@source $(DM_DIR)/lib.sh && dm_build $$@ $$^
 
